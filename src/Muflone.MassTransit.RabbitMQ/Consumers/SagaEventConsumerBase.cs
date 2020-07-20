a7c55055f40f636a2e14ff940a2c65da00a22cba
Alessandro Colla <alessandro.colla@outlook.com>
2020-07-20 11:43:07 +0200

    Implement consumerbase for saga's handlers

---
+++ src/Muflone.MassTransit.RabbitMQ/Consumers/SagaEventConsumerBase.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using MassTransit;
+using Muflone.Messages.Events;
+using Muflone.Saga;
+
+namespace Muflone.MassTransit.RabbitMQ.Consumers
+{
+	public abstract class SagaEventConsumerBase<TEvent> : IConsumer<TEvent> where TEvent : Event
+	{
+		protected abstract ISagaEventHandler<TEvent> Handler { get; }
+		public abstract Task Consume(ConsumeContext<TEvent> context);
+	}
+}
